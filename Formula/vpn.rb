c072676ff16d6fb6315de0287e9359ae9714e941
Liangquan Zhou <liangquanzhou@gmail.com>
2026-03-30 00:42:07 +0800

    chore: update vpn to v1.0.1

--- Formula/vpn.rb
+++ Formula/vpn.rb
@@ -1,8 +1,8 @@
 class Vpn < Formula
   desc "OpenConnect VPN connection manager with DNS healing and route cleanup"
   homepage "https://github.com/liangquanzhou/vpn"
-  url "https://github.com/liangquanzhou/vpn/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "371c9b9bb4914660760dc8224ccbf3452345d53129783a429cce6fe90ff770ef"
+  url "https://github.com/liangquanzhou/vpn/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "f96c281b10807192dfc4cc37021928434bc83dbe1fb77d94dff4f2cef4b82813"
   license "MIT"
 
   depends_on "openconnect"
