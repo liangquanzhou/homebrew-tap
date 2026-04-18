e18a956cf7df0091d762fc3e30cd7d8c5fe2f60d
Liangquan Zhou <liangquanzhou@gmail.com>
2026-04-19 02:05:52 +0800

    Bump autotask to v0.1.4

--- Formula/autotask.rb
+++ Formula/autotask.rb
@@ -4,8 +4,8 @@
 class Autotask < Formula
   desc "Personal macOS automation task registry and launchd manager"
   homepage "https://github.com/liangquanzhou/autotask"
-  url "https://github.com/liangquanzhou/autotask/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "67a85d12c67506de04072e67955d68282d1310d7d665cdd8a1cd4d13d91b5d09"
+  url "https://codeload.github.com/liangquanzhou/autotask/tar.gz/refs/tags/v0.1.4"
+  sha256 "70df4e1fb05fc3e677854fe654fba2b17f34dc026cec3313715ed2fecadff0eb"
   license "MIT"
 
   depends_on "go" => :build
@@ -15,6 +15,6 @@ class Autotask < Formula
   end
 
   test do
-    assert_match "0.1.3", shell_output("#{bin}/autotask version")
+    assert_match "0.1.4", shell_output("#{bin}/autotask version")
   end
 end
