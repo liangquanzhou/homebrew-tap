22f04757fa7f7855ecf9eefe9e534757f2295759
Liangquan Zhou <liangquanzhou@gmail.com>
2026-04-19 18:17:26 +0800

    Bump autotask to v0.1.6

--- Formula/autotask.rb
+++ Formula/autotask.rb
@@ -4,17 +4,25 @@
 class Autotask < Formula
   desc "Personal macOS automation task registry and launchd manager"
   homepage "https://github.com/liangquanzhou/autotask"
-  url "https://codeload.github.com/liangquanzhou/autotask/tar.gz/refs/tags/v0.1.5"
-  sha256 "c7102288d6eeeea9bedd7b742e21dcba8e7424e184d3eb1f5da84f96003631d9"
+  url "https://codeload.github.com/liangquanzhou/autotask/tar.gz/refs/tags/v0.1.6"
+  sha256 "015ddf1a9c8d3d3ff153b8916cd87472722cf284dffda8d526a22b0d8fdc089c"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     system "go", "build", "-trimpath", "-o", bin/"autotask", "."
+    system "make", "-C", "ui", "build"
+    prefix.install "ui/build/AutotaskMenu.app"
+    (bin/"autotask-menu").write <<~EOS
+      #!/bin/bash
+      open "#{prefix}/AutotaskMenu.app"
+    EOS
   end
 
   test do
-    assert_match "0.1.5", shell_output("#{bin}/autotask version")
+    assert_match "0.1.6", shell_output("#{bin}/autotask version")
+    assert_path_exists prefix/"AutotaskMenu.app/Contents/MacOS/AutotaskMenu"
+    assert_path_exists bin/"autotask-menu"
   end
 end
